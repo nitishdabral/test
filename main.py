baea1388d9a00dd5003d1a6c69da88f9f6c5e181
cr7 <nitishdabral@gmail.com>
2023-02-01 20:21:35 +0530

    first commit

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import urllib
 client = pymongo.MongoClient("mongodb+srv://nitish:"+urllib.parse.quote('Nitish@123')+"@cluster0.oqqzj85.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
+this is
